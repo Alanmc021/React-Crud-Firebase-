1463fd471d5e7172697b02c35663cb5a5bbe1b3b
Alan Martins <37884191+Alanmc021@users.noreply.github.com>
2019-09-09 16:15:43 -0300

    Add files via upload

---
+++ src/Firebase.js
@@ -0,0 +1,9 @@
+import firebase from "firebase";
+
+var firebaseConfig = {
+    //Configuração do Firebase aqui meu amigo dev !!! 
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
+  export default firebase
